a7c648e989cf807a8b15ccb412842b3653c01508
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-02 11:17:46 +0530

    code-change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">What's Your Loction</h1>
+        <p data-ns-test="project-description">
+          In this we have a group in which all friend's location is there
+        </p>
+      </>
+    );
+  }
 }
-
 
 export default App;
